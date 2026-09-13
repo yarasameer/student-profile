41840a85c812e6ba1f2f816e48f0ca8a8c846d78
yarasameer <obeidatyara795@gmail.com>
2026-09-13 17:40:47 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Yara Obeidat"
+major = "Computer Game design and development"
+university = "Jordan University of science and technology"
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
