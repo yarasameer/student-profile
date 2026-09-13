befa6d6e9ed5cbf454cb61da226648fd72c27252
yarasameer <obeidatyara795@gmail.com>
2026-09-13 17:42:09 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 name = "Yara Obeidat"
 major = "Computer Game design and development"
 university = "Jordan University of science and technology"
+graduation_year = 2028
 print("Student Profile")
 print("----------------")
 print("Name:", name)
 print("Major:", major)
 print("University:", university)
+print("Graduation Year:", graduation_year)
